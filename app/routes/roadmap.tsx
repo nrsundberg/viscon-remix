95280fc179a511d17114406bae34229442e770ad
Noah Sundberg <noah.sundberg@visionistinc.com>
2024-10-10 10:48:21 -0400

    bring in photos

--- app/routes/roadmap.tsx
+++ app/routes/roadmap.tsx
@@ -1,9 +1,28 @@
 import {Page} from "~/components/App";
+import type {MetaFunction} from "@remix-run/node";
+import brooksPic from "../images/react-router-remix-graphix.jpg";
+import spider from "../images/remix-react-router-spider-man.png";
+
+
+export const meta: MetaFunction = () => {
+    return [
+        { title: "Where is it going? - VISCON" },
+        { name: "description", content: "Is Remix gone forever?" },
+    ];
+};
 
 export default function () {
     return (
         <Page>
-            Todoplz
+            <img
+                src={brooksPic}
+                alt={"React and Remix combination graphic"}
+            />
+
+            <img
+                src={spider}
+                alt={"Spider-man meme"}
+            />
         </Page>
     )
 }
